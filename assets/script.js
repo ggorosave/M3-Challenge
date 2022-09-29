2fee37d7125ee3b8aaa0a0c823c9830a74e87bae
ggorosave <103039442+ggorosave@users.noreply.github.com>
2022-09-28 20:30:17 -0700

    added for loop to generate password

--- assets/script.js
+++ assets/script.js
@@ -2,26 +2,28 @@
 var generateBtn = document.querySelector("#generate");
 
 //Global variables and arrays
-var passLength = "";
+var passLength = 0;
 var passCharacters = [];
 var specialChars = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "\\", "/", ":", ";", "<", ">", ".", ",", "?"];
 var numericChars = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var passArray = [];
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  
+  var password = generatePassword(); 
 
   //Generates password given user inputs
   function generatePassword () {
-
+    
     //Asks user for password and displayserror message if password length is less than 8
     function getPassLength () {
       passLength = window.prompt("How many characters would you like your password to contain? (Password must contain at least 8 characters)");
       
-      if (passLength < 8) {
-        window.alert("Password must contain at least 8 characters. Please try again.")
+      if (passLength < 8 || passLength > 128) {
+        window.alert("Password must contain at least 8 characters but no more than 128 characters. Please try again.")
         getPassLength();
       }
 
@@ -67,12 +69,24 @@ function writePassword() {
       }
     }
     console.log(passCharacters);
-  }
+
+    //Start Here 
+    for (i = 0; i < passLength; i++) {
+      randomChar = passCharacters[Math.floor(Math.random() * passCharacters.length)];
+      passArray.push(randomChar);
+    }
+    console.log(passArray);
+    password = passArray.join("");
+    return password
+    };
+  
   var passwordText = document.querySelector("#password");
 
-  // passwordText.value = password;
+  passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
