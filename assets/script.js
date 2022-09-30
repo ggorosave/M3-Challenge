f44c3d8902c5e3b04210df31356d5d2f5e977cd1
ggorosave <103039442+ggorosave@users.noreply.github.com>
2022-09-30 15:08:09 -0700

    Made final touchups to the JavaScript

--- assets/script.js
+++ assets/script.js
@@ -2,43 +2,48 @@
 var generateBtn = document.querySelector("#generate");
 
 
-// Write password to the #password input
+// Writes password to the #password input
 function writePassword() {
-  
-  // Can I simplify this code rather than declaring the variable below?
-  var password = generatePassword(); 
-  
+
+  var password = generatePassword();
+
   //Generates password given user inputs
-  function generatePassword () {
-    
+  function generatePassword() {
+
     // Decalring these variables here resets their values each time the generatePassword function is run, which is what I want
     var passLength = 0;
     var passCharacters = [];
     var passArray = [];
-    
+
     //Character arrays
-    var specialChars = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "*", "-", "_", "+", "=", "|", ":", ";", "<", ">"];
-    var numericChars = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    var lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    
-    
-    //Asks user for password and displays error message if password length is less than 8 or greater than 128
-    function getPassLength () {
+    const specialChars = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "*", "-", "_", "+", "=", "|", ":", ";", "<", ">"];
+    const numericChars = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+    //Asks user for desired password length and displays error message if it is less than 8 or greater than 128
+    function getPassLength() {
       passLength = window.prompt("How many characters would you like your password to contain? (Password must contain at least 8 characters)");
-      
-      if (passLength < 8 || passLength > 128) {
+      if (!passLength) {
+        // Returns "exit" if user hits Cancel
+        return passLength = "exit";
+      } else if (passLength < 8 || passLength > 128) {
         window.alert("Password must contain at least 8 characters but no more than 128 characters. Please try again.")
         getPassLength();
       }
 
       return passLength;
     }
-    
+
+    //Runs function above
     getPassLength();
-    // delete this console log later
-    console.log(passLength);
-    
+
+    // Exits the code if the user hits Cancel --> see getPassLength function above
+    if (passLength === "exit") {
+      return password = "Oops! Please, try again.";
+    }
+
     //Function that adds array values to the passCharacters array
     function passCharsPush(userCheck, charArray) {
       if (userCheck) {
@@ -47,25 +52,28 @@ function writePassword() {
         }
       }
     }
-    //Checks if user wants special characters runs the passCharsPush the for specialChars
+
+    // Checks if user wants certain characters; runs passCharsPush if confirmed
+    // special character check
     let userSpecChar = window.confirm("Click OK to include special characters.");
     passCharsPush(userSpecChar, specialChars);
-   
-    
-    // Checks if user wants numeric characters and runs the passCharsPush the for numericChars
+
+    // numeric characters check
     let userNumChar = window.confirm("Click OK to include numeric characters.");
     passCharsPush(userNumChar, numericChars);
-        
-    // Checks if user wants lower case characters and runs the passCharsPush the for lowerCaseChars
+
+    // lowercase characters check
     let userLowCaseChar = window.confirm("Click OK to include lowercase characters.");
     passCharsPush(userLowCaseChar, lowerCaseChars);
 
-    // Checks if user wants uppercase characters and runs the passCharsPush the for upperCaseChars
+    // uppercase characters check 
     let userUpCaseChar = window.confirm("Click OK to include uppercase characters.");
     passCharsPush(userUpCaseChar, upperCaseChars);
-    
-    //DELETE THIS BEFORE SUBMISSION
-    console.log(passCharacters);
+
+    //Displays an alert if user chooses no characters for password
+    if (userSpecChar === false && userNumChar === false && userLowCaseChar === false && userUpCaseChar === false) {
+      window.alert("Your password must contain some type of character. Try again.");
+    }
 
     //Randomly selects a series of character from the passCharacters array and puts them in the randomChar array 
     for (i = 0; i < passLength; i++) {
@@ -73,22 +81,17 @@ function writePassword() {
       passArray.push(randomChar);
     }
 
-    //DELETE THIS BEFORE SUBMISSION
-    console.log(passArray);
-    
     //Converts the passArray to a string and assigns the new string to the password variable
     password = passArray.join("");
     return password
-    };
-  
+  };
+
   //Targets the text area in the HTML
   var passwordText = document.querySelector("#password");
-  
+
   //Displays the contents of the password variable in the text area
   passwordText.value = password;
 }
 
-// Add event listener to generate button -- runs the writePassword function when the "Generate Password" button is pushed
+// Event listener -- runs the writePassword function when the "Generate Password" button is clicked
 generateBtn.addEventListener("click", writePassword);
-
-
